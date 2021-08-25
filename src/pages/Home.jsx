c52d1467b6f2e879e9b396fafc1f93547da1851e
Septentrion50 <erwan56lecorre@gmail.com>
2021-08-25 23:37:12 +0200

    Remove KUTE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,61 +1,123 @@
-import KUTE from 'kute.js';
-
 const Home = () => {
-  const tween = KUTE.fromTo(
-    '#blob1',
-    { path: '#blob1' },
-    { path: '#blob2' },
-    { repeat: 999, duration: 3000, yoyo: true }
-  )
-
-  tween.start();
 
   return (
     <>
       <section className="blue">
         <h1>Pure CSS curves</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur tempore architecto perferendis nulla? Ullam, beatae iusto voluptatum illo natus numquam rerum enim. Placeat, sed. Eaque consequatur voluptas ab in iure.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          tempore architecto perferendis nulla? Ullam, beatae iusto voluptatum
+          illo natus numquam rerum enim. Placeat, sed. Eaque consequatur
+          voluptas ab in iure.
+        </p>
         <div className="curve"></div>
       </section>
-      <section> 
+      <section>
         <h1>Pure CSS curves</h1>
-        <p>Quasi tempore veniam quaerat architecto quod laboriosam asperiores modi vero exercitationem cupiditate, ab magni, atque inventore repudiandae? Soluta asperiores obcaecati nulla molestiae, qui fuga error quis neque et deleniti vitae?</p>
+        <p>
+          Quasi tempore veniam quaerat architecto quod laboriosam asperiores
+          modi vero exercitationem cupiditate, ab magni, atque inventore
+          repudiandae? Soluta asperiores obcaecati nulla molestiae, qui fuga
+          error quis neque et deleniti vitae?
+        </p>
       </section>
       <section className="bubble">
         <h1>Bubble</h1>
-        <p>Fugit eligendi aliquam necessitatibus? Ut, veniam quibusdam modi porro reiciendis totam eos neque vitae amet praesentium vel asperiores, doloribus consectetur minima ad consequuntur vero provident nesciunt possimus doloremque? Laboriosam, iusto?</p>
+        <p>
+          Fugit eligendi aliquam necessitatibus? Ut, veniam quibusdam modi porro
+          reiciendis totam eos neque vitae amet praesentium vel asperiores,
+          doloribus consectetur minima ad consequuntur vero provident nesciunt
+          possimus doloremque? Laboriosam, iusto?
+        </p>
       </section>
       <section className="dark">
         <h1>Bubble</h1>
-        <p>Fuga voluptate est ad quia, molestias quibusdam molestiae quas ullam laboriosam eveniet. Laboriosam error fuga ducimus harum, voluptatibus, atque saepe eligendi consectetur totam, ad fugit esse aliquid quisquam quasi voluptate?</p>
+        <p>
+          Fuga voluptate est ad quia, molestias quibusdam molestiae quas ullam
+          laboriosam eveniet. Laboriosam error fuga ducimus harum, voluptatibus,
+          atque saepe eligendi consectetur totam, ad fugit esse aliquid quisquam
+          quasi voluptate?
+        </p>
       </section>
       <section className="red">
         <h1>Shape divider</h1>
-        <p>Accusamus nam aliquid molestias eos odio asperiores autem totam excepturi dicta provident vel, optio distinctio cum minus possimus pariatur quaerat minima, exercitationem adipisci blanditiis debitis doloribus consequatur dolore. Consequuntur, ipsa.</p>
+        <p>
+          Accusamus nam aliquid molestias eos odio asperiores autem totam
+          excepturi dicta provident vel, optio distinctio cum minus possimus
+          pariatur quaerat minima, exercitationem adipisci blanditiis debitis
+          doloribus consequatur dolore. Consequuntur, ipsa.
+        </p>
         <div className="wave">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
           </svg>
         </div>
       </section>
       <div className="spacer layer1"></div>
       <section>
         <h1>Haikei</h1>
-        <p>Alias eius praesentium recusandae! Sint tenetur quas non ratione temporibus reiciendis error ab enim quos eos. Facere, id voluptate nam magni error, quis, temporibus modi eaque ullam praesentium soluta quam!</p>
+        <p>
+          Alias eius praesentium recusandae! Sint tenetur quas non ratione
+          temporibus reiciendis error ab enim quos eos. Facere, id voluptate nam
+          magni error, quis, temporibus modi eaque ullam praesentium soluta
+          quam!
+        </p>
       </section>
       <div className="spacer layer2 flip"></div>
       <section className="pink">
         <h1>Haikei Animated</h1>
-        <p>Vel in cum corporis enim aliquam error. Reiciendis similique tenetur praesentium repellat placeat ducimus ipsam illum tempore, nam maxime illo dolor eos corrupti nihil explicabo possimus non temporibus saepe a.</p>
-        <svg id="visual" viewBox="0 0 960 300" width="960" height="300" version="1.1"><g transform="translate(485.6550552283018 179.09373723552324)"><path id="blob1" d="M82.6 -86.3C103.9 -61.3 116 -30.6 112 -4C108 22.6 87.9 45.3 66.6 57.8C45.3 70.3 22.6 72.6 -1.1 73.7C-24.7 74.8 -49.5 74.5 -73 62C-96.5 49.5 -118.7 24.7 -123.2 -4.5C-127.7 -33.7 -114.4 -67.4 -90.9 -92.4C-67.4 -117.4 -33.7 -133.7 -1.5 -132.2C30.6 -130.6 61.3 -111.3 82.6 -86.3" fill="#BB004B"></path></g><g style={{visibility: "hidden"}} transform="translate(473.10849947518386 129.83819254969126)"><path id="blob2" d="M56.9 -56C70.4 -43.4 75.7 -21.7 82.5 6.8C89.4 35.4 97.7 70.7 84.2 92.4C70.7 114 35.4 122 5.4 116.6C-24.5 111.2 -49 92.4 -61.5 70.7C-74 49 -74.5 24.5 -76.2 -1.6C-77.8 -27.8 -80.6 -55.6 -68.1 -68.3C-55.6 -81 -27.8 -78.5 -3.1 -75.4C21.7 -72.4 43.4 -68.7 56.9 -56" fill="#BB004B"></path></g></svg>
+        <p>
+          Vel in cum corporis enim aliquam error. Reiciendis similique tenetur
+          praesentium repellat placeat ducimus ipsam illum tempore, nam maxime
+          illo dolor eos corrupti nihil explicabo possimus non temporibus saepe
+          a.
+        </p>
+        <svg
+          id="visual"
+          viewBox="0 0 960 300"
+          width="960"
+          height="300"
+          version="1.1"
+        >
+          <g transform="translate(485.6550552283018 179.09373723552324)">
+            <path
+              id="blob1"
+              d="M82.6 -86.3C103.9 -61.3 116 -30.6 112 -4C108 22.6 87.9 45.3 66.6 57.8C45.3 70.3 22.6 72.6 -1.1 73.7C-24.7 74.8 -49.5 74.5 -73 62C-96.5 49.5 -118.7 24.7 -123.2 -4.5C-127.7 -33.7 -114.4 -67.4 -90.9 -92.4C-67.4 -117.4 -33.7 -133.7 -1.5 -132.2C30.6 -130.6 61.3 -111.3 82.6 -86.3"
+              fill="#BB004B"
+            ></path>
+          </g>
+          <g
+            style={{ visibility: "hidden" }}
+            transform="translate(473.10849947518386 129.83819254969126)"
+          >
+            <path
+              id="blob2"
+              d="M56.9 -56C70.4 -43.4 75.7 -21.7 82.5 6.8C89.4 35.4 97.7 70.7 84.2 92.4C70.7 114 35.4 122 5.4 116.6C-24.5 111.2 -49 92.4 -61.5 70.7C-74 49 -74.5 24.5 -76.2 -1.6C-77.8 -27.8 -80.6 -55.6 -68.1 -68.3C-55.6 -81 -27.8 -78.5 -3.1 -75.4C21.7 -72.4 43.4 -68.7 56.9 -56"
+              fill="#BB004B"
+            ></path>
+          </g>
+        </svg>
       </section>
       <div className="spacer layer2"></div>
       <section>
         <h1>Nice Curves</h1>
-        <p>Sapiente laborum, tempora qui inventore dignissimos suscipit iste facilis delectus cumque libero cupiditate voluptates ipsam, ad, quod assumenda repudiandae recusandae blanditiis. Beatae quam repudiandae voluptate repellendus quia facere deserunt amet.</p>
+        <p>
+          Sapiente laborum, tempora qui inventore dignissimos suscipit iste
+          facilis delectus cumque libero cupiditate voluptates ipsam, ad, quod
+          assumenda repudiandae recusandae blanditiis. Beatae quam repudiandae
+          voluptate repellendus quia facere deserunt amet.
+        </p>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default Home;
